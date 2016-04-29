20e0a90353d76921226bf8cec7bd75dfb61ee47e
Luke Yang <yifeiyan@usc.edu>
2016-04-29 02:22:52 -0700

    error handling

--- app/views.py
+++ app/views.py
@@ -10,10 +10,13 @@ log_path ="/home/pi/Desktop/ee459/runtime/sprinkler.log"
 @app.route('/home')
 def index():
     status_str = ""
-    with open(log_path, "r") as logfd:
-        for line in logfd:
-            pass
-        status_str = line
+    try: 
+        with open(log_path, "r") as logfd:
+            for line in logfd:
+                pass
+            status_str = line
+    except IOError:
+        status_str = ""
     return render_template('home.html', status_str = status_str)
 
 
@@ -21,20 +24,21 @@ def index():
 @app.route('/setzone/<int:zonenum>')
 def set_zone(zonenum): 
     if (0 <= zonenum <= 8):
-        ser = serial.Serial('/dev/ttyUSB0', timeout = 2)
-        ser.write(str(zonenum))
+        try:
+            ser = serial.Serial('/dev/ttyUSB0', timeout = 2)
+            ser.write(str(zonenum))
 
+            response = ser.read(255)
+            if (len(response) > 0):
+                with open(log_path, "a") as logfd:
+                    d = datetime.datetime.now()
+                    timestr = d.strftime("%m/%d/%Y %H:%M:%S")
+                    logfd.write(timestr + " " + response.strip())
+                    logfd.write("\n")
 
-        response = ser.read(255)
-        if (len(response) > 0):
-#                print('Got: ' + response)
-            with open(log_path, "a") as logfd:
-                d = datetime.datetime.now()
-                timestr = d.strftime("%m/%d/%Y %H:%M:%S")
-                logfd.write(timestr + " " + response.strip())
-                logfd.write("\n")
-
-        ser.close()
+            ser.close()
+        except IOError:
+            pass
         print("Log: sent to USB port: " + str(zonenum))
     else:
         print("Warning: invalid zone number " + zonenum)
