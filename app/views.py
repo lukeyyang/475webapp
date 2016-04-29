18a6b29fd28a8cb92b83df77b2c690ab70f3b587
Luke Yang <yifeiyan@usc.edu>
2016-04-29 00:50:48 -0700

    about to test sent serial

--- app/views.py
+++ app/views.py
@@ -1,8 +1,22 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, redirect
 from .nav import nav
+import serial
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
+
+
+
+@app.route('/setzone/<int:zonenum>')
+def set_zone(zonenum): 
+    if (0 <= zonenum <= 8):
+        ser = serial.Serial('/dev/ttyUSB0')
+        ser.write(str(zonenum))
+        ser.close()
+        print("Log: sent to USB port: " + str(zonenum))
+    else:
+        print("Warning: invalid zone number " + zonenum)
+    return redirect(url_for('index'))
